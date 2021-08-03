21067abead5e100700fbc59fe1ae9fc18f265e74
dmvict <dm.vict.kr@gmail.com>
2021-08-03 13:09:51 +0300

    Rename files, change configs

--- src/Pre.js
+++ src/Pre.js
@@ -1,5 +1,4 @@
 const ChildProcess = require( 'child_process' );
-
 ChildProcess.execSync( 'npm i --production', { stdio : 'inherit', cwd : __dirname } );
 
 const { retry } = require( './Retry.js' );
