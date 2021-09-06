a6af7a1676985ced156193ac715b74f0116dd395
dmvict <dm.vict.kr@gmail.com>
2021-09-06 17:40:07 +0300

    Extend file `Runner.js`, fix fails on MacOS

--- src/Runner.js
+++ src/Runner.js
@@ -1,2 +1,24 @@
-require( process.argv[ 2 ] )
-process.on( 'exit', () => process.send( process.env ) );
+
+/* this branch exists because some MacOs NodeJs instances cannot send big messages */
+if( process.platform === 'darwin' )
+{
+  let initEnvs = Object.assign( Object.create( null ), process.env );
+
+  require( process.argv[ 2 ] )
+
+  process.on( 'exit', () =>
+  {
+    let result = Object.create( null );
+    for( let key in process.env )
+    if( ( !key in initEnvs ) || initEnvs[ key ] !== process.env[ key ] )
+    result[ key ] = process.env[ key ];
+
+    process.send( result );
+  });
+}
+else
+{
+  require( process.argv[ 2 ] )
+  process.on( 'exit', () => process.send( process.env ) );
+}
+
