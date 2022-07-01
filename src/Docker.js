cfbf36ba76a47db9145563882bf862af45c1ffc2
dmvict <dm.vict.kr@gmail.com>
2022-07-01 12:56:56 +0300

    Implement routine `imageBuild`

--- src/Docker.js
+++ src/Docker.js
@@ -34,6 +34,40 @@ function exists()
   return !_.error.is( execSyncNonThrowing( 'docker -v' ) );
 }
 
+//
+
+function imageBuild( actionPath, image )
+{
+  const docker = this;
+
+  _.assert
+  (
+    docker.exists(),
+    'Current OS has no Docker utility.\n'
+    + 'Please, visit https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#preinstalled-software\n'
+    + 'and select valid workflow runner.'
+  );
+
+  if( image === 'Dockerfile' )
+  {
+    const actionName = _.path.name( actionPath );
+    const imageName = `${ actionName }_repo:${ actionName }_tag`.toLowerCase();
+    const dockerfilePath = _.path.join( actionPath, 'Dockerfile' );
+    const command = `docker build -t ${ imageName } -f ${ dockerfilePath } ${ actionPath }`;
+    const build = execSyncNonThrowing( command );
+    if( _.error.is( build ) )
+    throw _.error.brief( build );
+
+    core.info( `Dockerfile for action : ${ dockerfilePath }.` );
+    core.info( command );
+    core.info( build.toString() );
+
+    return imageName;
+  }
+
+  _.assert( false, `The action does not support requested Docker image type "${ image }". Please, open an issue with the request for the feature.` );
+}
+
 // --
 // export
 // --
@@ -41,6 +75,7 @@ function exists()
 const Self =
 {
   exists,
+  imageBuild,
 };
 
 module.exports = Self;
