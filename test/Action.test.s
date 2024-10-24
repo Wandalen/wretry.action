5e82f309015dec609df6e39fc5c1f0e4bb20ec13
dmvict <dm.vict.kr@gmail.com>
2024-10-24 17:39:05 +0300

    Write test routine `retryWithOptionPreRetryCommand`

--- test/Action.test.s
+++ test/Action.test.s
@@ -885,6 +885,87 @@ function retryWithOptionRetryCondition( test )
 
 //
 
+function retryWithOptionPreRetryCommand( test )
+{
+  const context = this;
+  const a = test.assetFor( false );
+  const actionPath = a.abs( '_action/actions/wretry.action/v1' );
+  const testAction = 'dmvict/test.action@v0.0.2';
+  const execPath = `node ${ a.path.nativize( a.abs( actionPath, 'src/Main.js' ) ) }`;
+  const isTestContainer = _.process.insideTestContainer();
+
+  /* - */
+
+  a.ready.then( () =>
+  {
+    test.case = 'enought attempts, executing command between retries';
+    core.exportVariable( `INPUT_ACTION`, testAction );
+    core.exportVariable( `INPUT_WITH`, 'value : 0' );
+    core.exportVariable( `INPUT_ATTEMPT_LIMIT`, '4' );
+    core.exportVariable( `INPUT_PRE_RETRY_COMMAND`, 'echo "Executing pre_retry_command"' );
+    return null;
+  });
+
+  actionSetup();
+
+  a.shellNonThrowing({ currentPath : actionPath, execPath });
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    if( !isTestContainer )
+    test.ge( _.strCount( op.output, '::set-env' ), 3 );
+    test.identical( _.strCount( op.output, '::error::Wrong attempt' ), 3 );
+    test.identical( _.strCount( op.output, /::error::undefined.*Attempts exhausted, made 4 attempts/ ), 0 );
+    test.identical( _.strCount( op.output, /::error::.*Process returned exit code/ ), 0 );
+    test.identical( _.strCount( op.output, 'Success' ), 1 );
+    test.identical( _.strCount( op.output, 'Executing pre_retry_command' ), 4 );
+    return null;
+  });
+
+  /* */
+
+  a.ready.then( () =>
+  {
+    test.case = 'command between retries trows error, interrupting execution';
+    core.exportVariable( `INPUT_ACTION`, testAction );
+    core.exportVariable( `INPUT_WITH`, 'value : 0' );
+    core.exportVariable( `INPUT_ATTEMPT_LIMIT`, '4' );
+    core.exportVariable( `INPUT_PRE_RETRY_COMMAND`, 'exit 1' );
+    return null;
+  });
+
+  actionSetup();
+
+  a.shellNonThrowing({ currentPath : actionPath, execPath });
+  a.ready.then( ( op ) =>
+  {
+    test.notIdentical( op.exitCode, 0 );
+    if( !isTestContainer )
+    test.ge( _.strCount( op.output, '::set-env' ), 2 );
+    test.identical( _.strCount( op.output, '::error::Wrong attempt' ), 1 );
+    test.identical( _.strCount( op.output, /::error::undefined.*Attempts exhausted, made 4 attempts/ ), 0 );
+    test.identical( _.strCount( op.output, /::error::.*Process returned exit code/ ), 1 );
+    test.identical( _.strCount( op.output, 'Success' ), 0 );
+    return null;
+  });
+
+  /* - */
+
+  return a.ready;
+
+  /* */
+
+  function actionSetup()
+  {
+    a.ready.then( () => { a.fileProvider.filesDelete( a.abs( '.' ) ); return null; } );
+    a.ready.then( () => { a.fileProvider.dirMake( actionPath ); return null; } );
+    a.shell( `git clone ${ a.path.nativize( context.actionDirPath ) } ${ a.path.nativize( actionPath ) }` );
+    return a.ready;
+  }
+}
+
+//
+
 function retryWithExternalActionOnLocal( test )
 {
   const context = this;
@@ -1869,6 +1950,7 @@ const Proto =
     retryWithOptionAttemptLimit,
     retryWithOptionAttemptDelay,
     retryWithOptionRetryCondition,
+    retryWithOptionPreRetryCommand,
 
     retryWithExternalActionOnLocal,
     retryWithExternalActionOnRemote,
